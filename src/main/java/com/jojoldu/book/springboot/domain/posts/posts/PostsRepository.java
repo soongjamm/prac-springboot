a8213ea011c3d822f82973a08d472eaa2c1288eb
soongjamm <kimstz0303@gmail.com>
2020-02-27 23:12:40 +0900

    게시물 전체 조회

--- src/main/java/com/jojoldu/book/springboot/domain/posts/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/posts/PostsRepository.java
@@ -1,7 +1,16 @@
 package com.jojoldu.book.springboot.domain.posts.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
+
+    //@Query 어노테이션 안의 SQL문을 그 아래에 선언된 변수에 함수화 시킨다.
+    //FROM Posts p --> p는 Posts의 alias 이다.
+    //DESC = Descending. 내림차순
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 
 }
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,18 +1,31 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//@RequiredArgsConstructor는 final 혹은 @Nonnull 필드에 대해서 생성자를 생성한다.
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+
+    //Model은 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장
+    //컨트롤러에서 뷰로 객체를 전달해주는 것이다.
+    //addAttribute속 첫번째 인자 "posts"는 단지 뷰에서 사용할 변수 이름일 뿐이고, potato가 되어도 무관하다. 뷰에서 사용할 변수 이름이다.
     @GetMapping("/")
-    public String index() {
-         return "index";
+    public String index(Model model) {
+         model.addAttribute("posts", postsService.findAllDesc() );
+        return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save"; //posts-save 라는 mustache 파일을 리턴한다. 라우팅
     }
+
 }
