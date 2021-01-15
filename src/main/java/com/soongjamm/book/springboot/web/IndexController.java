4b03bb58d053b61153a563b8c8dabc7ec149b151
soongjamm <kimstz0303@gmail.com>
2021-01-15 23:20:17 +0900

    5장 Spring security & OAuth2로 구글 로그인 (어렵다)

--- src/main/java/com/soongjamm/book/springboot/web/IndexController.java
+++ src/main/java/com/soongjamm/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.soongjamm.book.springboot.web;
 
+import com.soongjamm.book.springboot.config.auth.dto.SessionUser;
 import com.soongjamm.book.springboot.service.posts.PostsService;
 import com.soongjamm.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,22 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
